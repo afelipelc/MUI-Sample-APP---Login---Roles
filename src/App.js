1431568a754f06718e163ce44ba6afbeacea1497
AFelipe <afelipelc@gmail.com>
2021-01-20 11:46:32 -0600

    init card

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import CarCard from './components/CarCard';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Paper className={classes.paper}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={3} >
+          <CarCard datos={{ nombre: 'Sentra 2021', marca: 'Nissan' }} />
+        </Grid>
+      </Grid>
+      
+    </Paper>
   );
 }
 
