5b53b592e2f33dc76ea3f417f746f0ede06081e6
AFelipe <afelipelc@gmail.com>
2021-01-20 12:56:38 -0600

    crear, editar y eliminar elementos

--- src/components/CarCard.js
+++ src/components/CarCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import Icon from '@material-ui/core/Icon';
@@ -6,6 +6,7 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles({
   root: {
@@ -24,34 +25,96 @@ const useStyles = makeStyles({
   },
 });
 
-export default function CarCard({ datos }) {
+export default function CarCard({ datos, id, onDelete }) {
   const classes = useStyles();
-  console.log(datos);
+
+  const [auto, setAuto] = useState(datos);
+  const [editar, setEditar] = useState(false);
+
+  const toogleEditar = () => {
+    // altenar editar
+    setEditar(!editar);
+  };
+
+  const handleChange = (e) => {
+    // actualizar el state
+    setAuto({
+      ...auto, // tomar lo que tiene auto
+      [e.target.name] : e.target.value, // actualizar la propiedad del auto (nombre del textField)
+    });
+  };
+
+  const handleSave = () => {
+    // se deben enviar los cambios al componente padre
+
+    // deshabilitar editar
+    setEditar(false);
+  }
+
+  const handleDelete = () => {
+
+    if (onDelete) { // si recibió la función para eliminar, invocarla
+      onDelete(id);
+    }
+  };
+
+  useEffect(() => {
+    setAuto({ ...datos });
+  }, [datos, id])
 
   return (
     <Card className={classes.root} variant="outlined">
       <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Auto
-        </Typography>
-        <Typography variant="h5" component="h2">
-          {datos.nombre}
-        </Typography>
+        {!editar && (
+          <>
+            <Typography className={classes.title} color="textSecondary" gutterBottom>
+              Auto
+            </Typography>
+            <Typography variant="h5" component="h2">
+              {auto.nombre}
+            </Typography>
+          </>
+        )}
+
+        {editar && (
+          <TextField
+            label="Nombre"
+            name="nombre"
+            value={auto.nombre}
+            onChange={handleChange}
+          />
+        )}
         <Typography className={classes.pos} color="textSecondary">
           Marca
         </Typography>
-        <Typography variant="h6" component="p">
-          {datos.marca}
-        </Typography>
+
+        {!editar && (
+          <Typography variant="h6" component="p">
+            {auto.marca}
+          </Typography>
+        )}
+        {editar && (
+            <TextField
+              label="Marca"
+              name="marca"
+              value={auto.marca}
+              onChange={handleChange}
+            />
+          )}
       </CardContent>
       <CardActions>
-        <IconButton color="primary" aria-label="Editar">
-          <Icon size="small">edit</Icon>
-        </IconButton>
-        <IconButton color="primary" aria-label="Guardar">
-          <Icon size="small">save</Icon>
-        </IconButton>
-        <IconButton color="secondary" aria-label="Eliminar" className={classes.marginLeftAuto}>
+        {!editar && (
+          <IconButton color="primary" aria-label="Editar" onClick={toogleEditar}>
+            <Icon size="small">edit</Icon>
+          </IconButton>
+        )}
+
+        {editar && (
+          <IconButton color="primary" aria-label="Guardar" onClick={handleSave}>
+            <Icon size="small">save</Icon>
+          </IconButton>
+        )}
+        <IconButton color="secondary" aria-label="Eliminar" className={classes.marginLeftAuto} onClick={handleDelete}>
           <Icon size="small">delete</Icon>
         </IconButton>
       </CardActions>
